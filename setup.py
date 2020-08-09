cd0fb0c382ffb4a2839e8720e53e33e65b5b0a7a
Michael van Tellingen <michael@mvantellingen.nl>
2020-08-09 08:56:15 +0200

    Add test infrastructure using the pytest-mypy-plugins
    This adds two tests which act more as a proof of concept of the test
    infra

--- setup.py
+++ setup.py
@@ -1,10 +1,19 @@
 from distutils.core import setup
 
+tests_require = [
+    "pytest>=6.0.0",
+    "pytest-mypy-plugins==1.4.0",
+    "coverage[toml]==5.2",
+]
+
+
 setup(
     name="lxml-stubs",
     version="0.1",
     package_data={"lxml-stubs": ["*.pyi"]},
     packages=["lxml-stubs"],
+    tests_require=tests_require,
+    extras_require={"test": tests_require},
     classifiers=[
         "License :: OSI Approved :: Apache Software License",
     ],
